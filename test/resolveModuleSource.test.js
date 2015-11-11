0553e7aa123377fea010816d365f7389e542ceff
Jeff Hanson <jshanson7@gmail.com>
2015-11-11 15:51:33 -0800

    more tests

---
+++ test/resolveModuleSource.test.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var resolve = require('path').resolve;
+var resolveModuleSource = require('../lib/resolveModuleSource');
+var testAppDir = resolve(__dirname, 'test-app');
+var testAppLibDir = resolve(__dirname, 'test-app/lib');
+var nodeModulesDir = resolve(__dirname, '../node_modules');
+
+function resolveSource(source) {
+  return resolveModuleSource(
+    source,
+    [nodeModulesDir, testAppDir, testAppLibDir],
+    ['.js', '.json']
+  );
+}
+
+describe('resolveModuleSource()', function () {
+  it('returns provided source for non-existant module', function () {
+    assert(resolveSource('foo') === 'foo');
+  });
+
+  it('returns provided source for relative module', function () {
+    assert(resolveSource('./foo') === './foo');
+  });
+
+  it('returns provided source for native module', function () {
+    assert(resolveSource('path') === 'path');
+    assert(resolveSource('fs') === 'fs');
+  });
+
+  it('returns node_modules path for external module', function () {
+    assert(resolveSource('mocha') === resolve(nodeModulesDir, 'mocha'));
+  });
+
+  it('returns local path for local module', function () {
+    assert(resolveSource('models/User') === resolve(testAppDir, 'models/User'));
+    assert(resolveSource('utils') === resolve(testAppLibDir, 'utils'));
+  });
+});
